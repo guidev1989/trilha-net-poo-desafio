80e063f0f0a66498c5f9f8eb2a3a9bf83a6e816a
guidev1989 <guidev1989@gmail.com>
2023-12-19 07:06:52 -0300

    implementado modificações do desafio
     as classes Nokia e Iphone recebem os valores de Modelo, IMEI e Memoria através do construtor e não possuem a capacidade de alterar esses valores após a criação do objeto, conforme solicitado. Esses campos são definidos como privados (private set) e só podem ser definidos no momento da construção do objeto Smartphone.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,82 @@
-namespace DesafioPOO.Models
+using System;
+
+abstract class Smartphone
 {
-    public abstract class Smartphone
+    public string Numero { get; set; }
+    public string Modelo { get; private set; }
+    public string IMEI { get; private set; }
+    public int Memoria { get; private set; }
+
+    public Smartphone(string modelo, string imei, int memoria)
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        Modelo = modelo;
+        IMEI = imei;
+        Memoria = memoria;
+    }
 
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+    public abstract void Ligar();
+    public abstract void ReceberLigacao();
+    public abstract void InstalarAplicativo(string nome);
+}
 
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
+class Nokia : Smartphone
+{
+    public Nokia(string modelo, string imei, int memoria) : base(modelo, imei, memoria)
+    {
+    }
 
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
+    public override void Ligar()
+    {
+        Console.WriteLine("Ligando Nokia...");
+    }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+    public override void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação no Nokia...");
+    }
+
+    public override void InstalarAplicativo(string nome)
+    {
+        Console.WriteLine($"Instalando aplicativo {nome} no Nokia...");
+    }
+}
+
+class Iphone : Smartphone
+{
+    public Iphone(string modelo, string imei, int memoria) : base(modelo, imei, memoria)
+    {
+    }
+
+    public override void Ligar()
+    {
+        Console.WriteLine("Ligando iPhone...");
+    }
+
+    public override void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação no iPhone...");
+    }
+
+    public override void InstalarAplicativo(string nome)
+    {
+        Console.WriteLine($"Instalando aplicativo {nome} no iPhone...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Nokia nokia = new Nokia("Nokia XYZ", "987654321", 64);
+        Iphone iphone = new Iphone("iPhone 12", "123456789", 128);
+
+        // Exemplo de uso
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
     }
 }
